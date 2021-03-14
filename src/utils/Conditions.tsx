9dcb2d4d8afe7f46b7645a9aaca83c6096791e7c
Carlos <carlosaugustocf99@gmail.com>
2021-03-14 17:12:19 -0300

    Coditions icons

---
+++ src/utils/Conditions.tsx
@@ -0,0 +1,35 @@
+
+
+
+export function condition(condition : 'storm' | 'clear_day' | 'rain') : iconConditions | undefined{
+    let icon = null
+
+    switch(condition){
+        case 'storm':
+            return icon ={
+                name: 'rainy-outline',
+                color: '#1ec9ff'
+            };
+            break;
+        case 'clear_day':
+            return icon = {
+                name:  'partly-sunny-outline',
+                color: '#FFB300'
+            }
+        case 'rain' : 
+            return icon = {
+                name: 'rainy-outline',
+                color: '#1ec9ff'
+            } 
+        default:
+            return icon ={
+                name: 'cloud-outline',
+                color: '#1ec9ff'
+            }
+    }
+}
+
+interface iconConditions {
+    name: string;
+    color: string;
+}
